18be36bc451b863047042291c2e9cbc73ff5cc9a
panthera-dark <panthera.dark@hotmail.com>
2020-04-03 18:19:35 +0200

    Fix the changes for heroku

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import Banner from './Banner/Banner';
-import TopCardHolder from '../Cardholder/TopCardHolder';
-import Section from './Section/Section';
+import Banner from "./Banner/Banner";
+import TopCardHolder from "../Cardholder/TopCardHolder";
+import Section from "./Section/Section";
 
 class LandingPage extends Component {
-
-    render() {
-        return (
-            <div>
-                <Banner />
-                <Section />
-                <TopCardHolder/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Banner />
+        <Section />
+        <TopCardHolder />
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
