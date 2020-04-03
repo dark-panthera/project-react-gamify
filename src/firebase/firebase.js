94ceef5237aa475d93b94419550865e86b20c021
panthera-dark <panthera.dark@hotmail.com>
2020-04-03 19:02:47 +0200

    refactored the files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from "firebase";
-
+/*
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN, 
@@ -9,6 +9,18 @@ const firebaseConfig = {
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId:process.env.FIREBASE_APPID,
   measurementId:process.env.FIREBASE_MEASUREMENT_ID
+};
+*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1MtrFmJ5jRLf0KCGTcTS5WefbjRB-2ts",
+  authDomain: "react-gamer.firebaseapp.com",
+  databaseURL: "https://react-gamer.firebaseio.com",
+  projectId: "react-gamer",
+  storageBucket: "react-gamer.appspot.com",
+  messagingSenderId: "1011339585772",
+  appId: "1:1011339585772:web:f17a7f24ddc33b04e35dc9",
+  measurementId: "G-45LZ2BRVQ1"
 };
 
 firebase.initializeApp(firebaseConfig);
